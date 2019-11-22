75a7497944e66496bb0d1b85ce0f63e2c4a9251e
smed <slimani.med.21@gmail.com>
2019-11-22 07:33:58 -0800

    User entity

---
+++ api/src/entities/user/user.js
@@ -0,0 +1,43 @@
+const NOIMAGE = "https://www.clipartwiki.com/clipimg/detail/197-1979569_no-profile.png"
+
+export default function BuildMakeUser({Id, crypt, validator}) {
+    return async function makeUser({
+                                       id = Id.makeId(),
+                                       name,
+                                       pwd,
+                                       email,
+                                       imgUrl = NOIMAGE,
+                                       googleId = null,
+                                       approved = false
+                                   }) {
+        if (!name) {
+            throw new Error("User must have a Name")
+        }
+        if (!pwd) {
+            throw new Error('User must have a password')
+        }
+        if (!validator.validate(email)) {
+            throw new Error("user must have a valid email")
+        }
+        if (!Id.isValidId(id)) {
+            throw new Error("user ID id not valid")
+        }
+
+        let encryptedPwd = await crypt(pwd);
+
+        return Object.freeze({
+            getName: () => name,
+            getPwd: () => encryptedPwd,
+            getEmail: () => email.toLowerCase(),
+            getId: () => id,
+            getGoogleId: () => googleId,
+            approve: () => {
+                approved = true;
+            },
+            disapprove: () => {
+                //We can use this to bad users that misbehave
+                approved = false;
+            }
+        })
+    }
+}
