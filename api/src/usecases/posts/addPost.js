791bdea820992efd8d19fa430620c487cd5fd5d5
smed <slimani.med.21@gmail.com>
2019-12-05 20:51:34 +0100

    Implementing the Add POst Use Case

---
+++ api/src/usecases/posts/addPost.js
@@ -0,0 +1,24 @@
+import makePost from "../../entities/post";
+
+export default function makeAddPost({postDb}){
+    return async function addPost(postInfo){
+        const post = makePost(postInfo);
+
+        // in here we can monitor the post, the image, and send stuff to the AI
+        // if everything is okay
+        post.approve();
+
+        return postDb.insert({
+            id: post.getId(),
+            imgUrl: post.getImgUrl(),
+            authorId: post.getAuthorId(),
+            des: post.getDes(),
+            dateAdded: post.getDateAdded(),
+            location: post.getLocation(),
+            approved: post.getApproved(),
+            usersPended: post.getUsersPended(),
+            usersApproved : post.getUsersApproved(),
+            usersRejected: post.getUsersRejected()
+        })
+    }
+}
