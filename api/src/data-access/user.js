12e8611dae956bf77e2fe9ba155b04729eb0b20e
smed <slimani.med.21@gmail.com>
2019-12-06 22:40:30 +0100

    User Db skeleton

---
+++ api/src/data-access/user.js
@@ -0,0 +1,24 @@
+export default function makeUsersDb (){
+    return Object.freeze({
+        findById,
+        findByEmail
+        update,
+        insert,
+        remove
+    });
+    async function remove(){
+        throw new Error('To be implemented!')
+    }
+    async function findById(){
+        throw new Error('To be implemented!')
+    }
+    async function findByEmail(){
+        throw new Error('To be implemented!')
+    }
+    async function insert(){
+        throw new Error('To be implemented!')
+    }
+    async function update(){
+        throw new Error('To be implemented!')
+    }
+}
