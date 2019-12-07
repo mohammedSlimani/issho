60425d2f3df8adebb2ef3d490c77cd2a72b7fb7c
smed <slimani.med.21@gmail.com>
2019-12-07 18:37:22 +0100

    Edit User usecase

---
+++ api/src/usecases/users/editInfo.js
@@ -0,0 +1,27 @@
+import {makeUser} from "../../entities";
+
+export default function makeEditInfo ({usersDb}) {
+    return async function editInfo({id, changes}){
+        if(!id){
+            throw new Error("You have to supply the id of the user you want to edit ");
+        }
+
+        const existingUser = await usersDb.findById({id});
+        if(!existingUser){
+            throw new Error("The user you want to edit doesnt exist");
+        }
+
+        const updatedUser = await makeUser({id: id, existingUser, ...changes});
+
+        return usersDb.update({
+            id:updatedUser.getId(),
+            name : updatedUser.getName(),
+            pwd: updatedUser.getPwd(),
+            email : updatedUser.getEmail(),
+            imgUrl : updatedUser.getImgUrl(),
+            googleId : updatedUser.getGoogleId(),
+            approved : updatedUser.getApproved(),
+            deleted : updatedUser.getDeleted()
+        })
+    }
+}
