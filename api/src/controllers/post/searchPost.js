30e80d92e9d541be7f7f58845e14d5554bb190cb
smed <slimani.med.21@gmail.com>
2019-12-07 20:54:57 +0100

    Search Post controller

---
+++ api/src/controllers/post/searchPost.js
@@ -0,0 +1,29 @@
+export default function makeSearchPostController({searchPost}) {
+    return async function searchPostController(httpRequest){
+        const headers = {
+            "Content-Type": "multipart/form-data"
+        };
+        try {
+            const {
+                queries
+            } = httpRequest.body;
+
+            const post = await searchPost(queries);
+
+            return {
+                headers,
+                statusCode: 201,
+                body: { ...post }
+            };
+        } catch (e) {
+            console.log(e);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    error: e.message
+                }
+            };
+        }
+    }
+}
