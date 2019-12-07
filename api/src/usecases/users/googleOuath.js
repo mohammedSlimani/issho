139d7dcdbaf8f7a99f1c3cbe13c24de9dd89ecaa
smed <slimani.med.21@gmail.com>
2019-12-07 18:37:14 +0100

    GoogleOuath instead of facebook Ouath

---
+++ api/src/usecases/users/googleOuath.js
@@ -0,0 +1,43 @@
+import {makeUser} from "../../entities";
+
+export default function makeGoogleOuath({usersDb}) {
+    return async function googleOuath({email, googleId, name, image}){
+        if(!googleId){
+            throw new Error("You hva to provide the google Id of the user");
+        }
+        if(!image){
+            throw new Error("the googleOuath should contain an Image");
+        }
+
+        const exists = await usersDb.findByEmail({email});
+        if(exists){
+            //then an already existing user is trying to link his googleAccount
+            return usersDb.update({
+                id: exists.id,
+                googleId,
+                image
+            })
+        }else{
+            //we are dealing with a new user
+            const user = await makeUser({
+                email,
+                name,
+                googleId,
+                imgUrl: image,
+                pwd:""
+            });
+            user.approve();
+
+            return usersDb.insert({
+                id:updatedUser.getId(),
+                name : updatedUser.getName(),
+                pwd: updatedUser.getPwd(),
+                email : updatedUser.getEmail(),
+                imgUrl : updatedUser.getImgUrl(),
+                googleId : updatedUser.getGoogleId(),
+                approved : updatedUser.getApproved(),
+                deleted : updatedUser.getDeleted()
+            })
+        }
+    }
+}
