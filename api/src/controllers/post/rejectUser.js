0b7df60b31dbba8b38630dc8fbfaa71eace51b46
smed <slimani.med.21@gmail.com>
2019-12-07 20:46:33 +0100

    RejectUser controller

---
+++ api/src/controllers/post/rejectUser.js
@@ -0,0 +1,30 @@
+export default function makeRejectUserController({rejectUser}) {
+    return async function rejectUserController(httpRequest){
+        const headers = {
+            "Content-Type": "multipart/form-data"
+        };
+        try {
+            const {
+                postId,
+                userId
+            } = httpRequest.body;
+
+            const post = await rejectUser({postId, userId});
+
+            return {
+                headers,
+                statusCode: 201,
+                body: { ...post }
+            };
+        } catch (e) {
+            console.log(e);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    error: e.message
+                }
+            };
+        }
+    }
+}
