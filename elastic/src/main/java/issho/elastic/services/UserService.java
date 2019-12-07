e5fb1ec4ddc302cda5b38a90fc72ea8bb3fb96b7
mouhcineToumi <mouhcine13.mt@gmail.com>
2019-12-07 20:39:28 +0100

    GET in services

--- elastic/src/main/java/issho/elastic/services/UserService.java
+++ elastic/src/main/java/issho/elastic/services/UserService.java
@@ -1,4 +1,60 @@
 package issho.elastic.services;
 
-public class UserService {
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService extends ElasticCrud {
+
+    public UserService(String index) {
+        super(index);
+    }
+
+
+    public String getByEmail(String email) throws IOException {
+
+        SearchRequest searchRequest = new SearchRequest(this.index);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        BoolQueryBuilder query = QueryBuilders.boolQuery()
+                .filter(QueryBuilders.termsQuery("email", email));
+
+        searchSourceBuilder.query(query);
+        searchRequest.source(searchSourceBuilder);
+
+        SearchHits searchHits = this.client.search(searchRequest, RequestOptions.DEFAULT).getHits();
+        List<String> hits = new ArrayList<String>();
+        for (SearchHit hit : searchHits) {
+            hits.add(hit.getSourceAsString());
+        }
+        return hits.get(0);
+    }
+
+    public String getByName(String name) throws IOException {
+
+        SearchRequest searchRequest = new SearchRequest(this.index);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        BoolQueryBuilder query = QueryBuilders.boolQuery()
+                .filter(QueryBuilders.termsQuery("name", name));
+
+        searchSourceBuilder.query(query);
+        searchRequest.source(searchSourceBuilder);
+
+        SearchHits searchHits = this.client.search(searchRequest, RequestOptions.DEFAULT).getHits();
+        List<String> hits = new ArrayList<String>();
+        for (SearchHit hit : searchHits) {
+            hits.add(hit.getSourceAsString());
+        }
+        return hits.get(0);
+
+    }
 }
