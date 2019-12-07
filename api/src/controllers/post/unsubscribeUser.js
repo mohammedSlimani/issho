5eba5ddb38ca8367aaa8766d0ede98ab7788a4da
smed <slimani.med.21@gmail.com>
2019-12-07 20:48:19 +0100

    unsubscribeUser controller

---
+++ api/src/controllers/post/unsubscribeUser.js
@@ -0,0 +1,30 @@
+export default function makeUnsubscribeUserController({unsubscribeUser}) {
+    return async function unsubscribeUserController(httpRequest){
+        const headers = {
+            "Content-Type": "multipart/form-data"
+        };
+        try {
+            const {
+                postId,
+                userId
+            } = httpRequest.body;
+
+            const post = await unsubscribeUser({postId, userId});
+
+            return {
+                headers,
+                statusCode: 201,
+                body: { ...post }
+            };
+        } catch (e) {
+            console.log(e);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    error: e.message
+                }
+            };
+        }
+    }
+}
