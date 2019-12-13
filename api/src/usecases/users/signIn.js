bd75439b823b3bf29cd7385a4e39b9f427e14862
smed <slimani.med.21@gmail.com>
2019-12-13 19:18:28 +0100

    refector

--- api/src/usecases/users/signIn.js
+++ api/src/usecases/users/signIn.js
@@ -3,7 +3,7 @@ export default function makeSignIn({usersDb, compare}){
         const exist = await usersDb.findByEmail({ email: userInfo.email });
         if (exist && Object.entries(exist).length !== 0) {
             //todo: fix this bad naming convention
-            if (await compare(userInfo.pwd, exist.password)) {
+            if (await compare(userInfo.pwd, exist.pwd)) {
                 return exist;
             }
             throw new Error("Wrong password");
